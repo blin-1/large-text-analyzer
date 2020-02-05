2777165539b5a8cec991bdd8aba4a2e3c1a43634
blin-1 <y.monsoon@gmail.com>
2020-02-05 16:07:32 -0500

    readme

--- index.mjs
+++ index.mjs
@@ -1,7 +1,5 @@
 import lta from './modules/large-text-analyzer.mjs'
-
 count('./test/data/testdata.txt')
-
 async function count (fileName) {
 	let wordMap = await lta.getWordMap(fileName);
 	console.log(`Vocabulary consists of ${wordMap.size} words`)
