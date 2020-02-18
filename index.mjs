4401f3bd070f647e0194c3c581b34b43c876e12f
blin-1 <y.monsoon@gmail.com>
2020-02-17 21:51:17 -0500

    Override default processing

--- index.mjs
+++ index.mjs
@@ -1,13 +1,26 @@
 import lta from './modules/large-text-analyzer.mjs'
 
-// Optionally set the word delimiters:
-// (Default is /\s|[^a-zA-Z]|[0-9]/ but that doesn't begin to touch unicode of course)    
-// lta.delimiters = /\s/             
+// OPTIONALLY override the word delimiters - this RegEx is the default one:
 
-count('./test/data/testdata.txt')
+lta.delimiters = /\s|[^a-zA-Z]|[0-9]/ 
 
-async function count (fileName) {
-	let wordMap = await lta.getWordMap(fileName);
-	console.log(`Vocabulary consists of ${wordMap.size} words`)
-	console.log(wordMap);
+// OPTIONALLY override the processWord(word) function to be executed for each word - 
+// this here is the default - counts unique word occurences 
+// NOTE "this.map" - this is the result map, that gets returned at the end of the stream
+
+lta.processWord = function (word){           		
+		word = word.toLowerCase()		
+		let count = this.map.get(word)
+		count = count? ++count : 1
+		this.map.set(word,count)
 }
+
+// Define async block:
+async function processFile (fileName) {
+	let vocabulary = await lta.processWords(fileName);
+	console.log(`Vocabulary consists of ${vocabulary.size} words`)
+	console.log(vocabulary);
+}
+
+// And execute it.
+processFile('./test/data/testdata.txt')
--- modules/large-text-analyzer.mjs
+++ modules/large-text-analyzer.mjs
@@ -2,29 +2,40 @@ import fs from 'fs';
 
 class LargeTextAnalyzer {
 
+	static map = new Map();
 	static delimiters = /\s|[^a-zA-Z]|[0-9]/
+	static processWord = function (word) {									
+			word = word.toLowerCase()
+			let count = this.map.get(word)
+			count = count? ++count : 1
+			this.map.set(word,count)
+	} 
 
 	/**
 	 * @param {any} delimiters
 	 */
 	set delimiters (delimiters) {
-         this.delimiters = delimiters;
+         this.delimiters = delimiters
+	}
+	
+	/**
+	 * The function to be executed for each word
+	 * @param {function} processWord
+	 */
+	set processWord (processWord) {
+         this.processWord = processWord
     }
 
-	static getWordMap (fileName) {
+	static processWords (fileName) {
 		
-		let map = new Map()
 		return new Promise((resolve) => {
 				let stream = fs.createReadStream(fileName)
 				stream.on("readable", () => {
 							let character, word = ''	
 							while (character = stream.read(1)) {
 								if (this.delimiters.test(character)){
-									if (word){
-										word = word.toLowerCase()
-										let count = map.get(word)
-										count = count? ++count : 1
-										map.set(word,count)
+									if (word) {
+										this.processWord(word)
 										word = ''
 									}
 								}else {
@@ -33,7 +44,7 @@ class LargeTextAnalyzer {
 							}
 				})
 				stream.on("end", () => {
-					resolve(new Map([...map].sort()))
+					resolve(new Map([...this.map].sort()))
 				})
 			})
 	}
